23bec03bd10a7caa86cded3578a5d2b0fd7f4a8f
ninintin <ninnin.webdev@gmail.com>
2023-08-16 18:38:02 +0600

    Resolved merge conflicts in devs branch

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,7 +14,7 @@ import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
 const streamReports = ref([]);
-
+// Hi i would like to change this
 onMounted(async () => {
   try {
     const response = await axios.get('http://51.159.111.59:8085/api/admin/stream-reports?page=1');
