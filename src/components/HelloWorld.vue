0fc29f9a2c35574394088e389b6296d686ddb2d7
ninintin <ninnin.webdev@gmail.com>
2023-08-16 18:35:17 +0600

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Stream Reports</h1>
+    <ul>
+      <li v-for="report in streamReports" :key="report.id">
+        {{ report.title }} - {{ report.description }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const streamReports = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://51.159.111.59:8085/api/admin/stream-reports?page=1');
+    streamReports.value = response.data; // Assuming the API response is an array of objects
+  } catch (error) {
+    console.error('Error:', error);
+  }
+});
+</script>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Stream Report Details</h1>
+    <div v-if="streamReportDetails">
+      <h2>{{ streamReportDetails.title }}</h2>
+      <p>{{ streamReportDetails.description }}</p>
+      <!-- Display other details as needed -->
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const streamReportDetails = ref(null);
+
+onMounted(() => {
+  axios.get('http://127.0.0.1:8000/api/admin/stream-reports/2')
+    .then(response => {
+      streamReportDetails.value = response.data;
+    })
+    .catch(error => {
+      console.error('Error fetching stream report details:', error);
+    });
+});
+</script>
